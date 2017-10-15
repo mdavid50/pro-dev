d527841f7aa6d755fc753691d4585d11fbf82d9f
Matt David <mcdav3636@gmail.com>
2017-10-15 15:18:44 -0600

    added content

--- index.php
+++ index.php
@@ -2,9 +2,27 @@
 <html>
 	<head>
 	<meta charset="utf-8">
-	<title></title>
+	<title>Matthew Davids ProDev Doc</title>
 	</head>
 	<body>
-		<h1></h1>
+		<h1>Matthew David Professional Developement</h1>
+
+		<p><strong>Last Updated:</strong> 10/15/17</p>
+
+		<h2>My Links</h2>
+		<ul>
+			<li></li>
+		</ul>
+		<h2>Homework Activities</h2>
+			<h3>My Learning Styles</h3>
+			<p><strong>Visual</strong></p>
+			<p>50% Visual</p>
+			<p>30% Audible</p>
+			<p>20% Tactile</p>
+
+			<h3>Quantative Accomplishments</h3>
+		<ul>
+			<li></li>
+		</ul>
 	</body>
 </html>
