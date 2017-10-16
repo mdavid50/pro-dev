3c898443717dec7d110605795785d71a06006429
Matt David <mcdav3636@gmail.com>
2017-10-15 22:28:03 -0600

    added github link, added reflections section.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 	<meta charset="utf-8">
-	<title>Matthew Davids ProDev Doc</title>
+	<title>Matthew David ProDev Doc</title>
 	</head>
 	<body>
 		<h1>Matthew David Professional Developement</h1>
@@ -12,7 +12,7 @@
 
 		<h2>My Links</h2>
 		<ul>
-			<li></li>
+			<li><a href="https://github.com/mdavid50">GitHub</a></li>
 		</ul>
 		<h2>Homework Activities</h2>
 			<h3>My Learning Styles</h3>
@@ -47,5 +47,27 @@
 			<p> Hi my name is Matt David. I am learning to be an effective development team member so I can
 			be a confident remote employee. I'm building a solid coding foundation with good habits to be a
 			valued developer. Does your company believe in growth from within?</p>
+
+		<h2>Reflections</h2>
+
+			<h3>Week 1</h3>
+			<p>What to think!!! I'm in a totally different world. I'm not sure what to make of it right now. My
+			biggest challenges are not with code itself or the learning style but getting used to the environment it
+				happens in. I'm used to having multiple problems a day. When a problem presents itself I must handle it
+				quickly and efficiently so I may travel to the next issue on my plate. Everything in the world I'm used to
+				has hard and fast deadlines.
+			No excuse is acceptable and to have any problem for an extended period of time would be detrimental to
+			your job and future. I'm used to having a problem that I can touch and manipulate. Doing actions by
+			giving commands to a piece of technology is a brand new concept for me. Don't get me wrong, I am no
+			stranger to typing a paper or making a small add. I'm frequently cruising the internet as many in this
+			day and age do but until recently I have not had a purpose for a computer in my life. The ailments of
+			my usual world are cuts, calluses, and bruises. The activities are to build and do. This new world is
+			different. The biggest pain is headache! The activities are to dream and create! I know at this moment I
+			have very little skill in this unfamiliar place but I am more than capable of learning anything in this
+			world with proper effort. I will have to work harder than most in my class to gain the knowledge that
+			some already seem versed in but I know I will not give up. I will make a place for myself in this new
+			adventure. It is the most pleasant way I have spent a normal work day I can remember. I'm excited to see
+			what is to come.</p>
+
 	</body>
 </html>
