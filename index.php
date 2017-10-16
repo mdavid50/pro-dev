8e59021659788d30babde766ddea355a6b779121
Matt David <mcdav3636@gmail.com>
2017-10-15 20:12:36 -0600

    added quantitative, qualative, golden circle, and elevator pitch

--- index.php
+++ index.php
@@ -20,9 +20,31 @@
 			<p>30% Audible</p>
 			<p>20% Tactile</p>
 
-			<h3>Quantative Accomplishments</h3>
+			<h3>Quantitative Accomplishments</h3>
 		<ul>
-			<li></li>
+			<li>Worked 70hr mandatory work weeks for over 6 months to accomplish a project before funding deadlines.</li>
+			<li>Reduced man hours by 28% on my crew by working without an apprentice.</li>
+			<li>Survived 19 layoff periods in a four year span</li>
 		</ul>
+
+			<h3>Qualitative Features</h3>
+		<ul>
+			<li>Deeply dedicated to accomplishing goals.</li>
+			<li>Confident and skilled communicator.</li>
+			<li>Very Capeable Learner</li>
+		</ul>
+
+			<h3>Golden Circle</h3>
+			<ul>
+				<li><strong>Why:</strong> To live where I want, how I want.</li>
+				<li><strong>How:</strong> Become a compeent coder with a strong and broad foundationt</li>
+				<li><strong>What:</strong> Learn how to work with a team effectively so that I will be a confident
+					remote employee.</li>
+			</ul>
+
+			<h3>Elevator Pitch</h3>
+			<p> Hi my name is Matt David. I am learning to be an effective development team member so I can
+			be a confident remote employee. I'm building a solid coding foundation with good habits to be a
+			valued developer. Does your company believe in growth from within?</p>
 	</body>
 </html>
