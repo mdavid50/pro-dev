f41d79e86b240d89dc72b85e41700dee63a54c9b
Matt David <mcdav3636@gmail.com>
2017-10-23 15:39:20 -0600

    add resume and made link

--- index.php
+++ index.php
@@ -10,6 +10,8 @@
 		<p><strong>Last Updated:</strong> 10/23/17</p>
 		<p><strong>Coding Buddy:</strong> Neena</p>
 
+        <a href="resume/resume.pdf">Resume</a>
+
 		<h2>My Links</h2>
 		<ul>
 			<li><a href="https://github.com/mdavid50">GitHub</a></li>
